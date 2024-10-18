ae6b04de531c652f5301ea97b278b81730ceb6f6
Arek <arek0006@gmail.com>
2024-10-18 16:39:11 +0200

    Projectile Weapon Class

---
+++ Source/Blaster/Weapon/Projectile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class BLASTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UBoxComponent> CollisionBox;
+};
